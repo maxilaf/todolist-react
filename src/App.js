5807076ec166b41d06619cfd4fc43b14a980a5da
maxilaf <maxilafxl@gmail.com>
2024-06-22 12:35:47 +0200

    Todolist

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   // state (état, données)
-  const [taches, setTaches] = useState([{ id: 1, nom: "Pomme" },
-    { id: 2, nom: "Ananas" },
-    { id: 3, nom: "Pastèque" },]);
+  const [taches, setTaches] = useState([{ nom: "Pomme" },
+    { nom: "Ananas" },
+    { nom: "Pastèque" },
+  ]);
 
-  const inputRef = useRef();
+  const [nouvelleTache, setNouvelleTache] = useState("");
+
+  //const inputRef = useRef();
 
   // comportements
   const addTaches = (event) => {
     event.preventDefault();
-    const tachesLenght = taches.length + 1;
-    const newTaches = { id: tachesLenght, nom: inputRef.current.value };
-    setTaches([...taches, newTaches]);
-    console.log(taches);
+    if (nouvelleTache !== "") {
+      const newTaches = { nom: nouvelleTache};
+      setTaches([...taches, newTaches]);
+      setNouvelleTache("");
+    }
+    // const tachesLenght = taches.length + 1;
+    // const newTaches = { id: tachesLenght, nom: inputRef.current.value };
+    // setTaches([...taches, newTaches]);
+    // console.log(taches);
   };
 
-  const deleteLi = (id) => {
+  const deleteLi = (tache) => {
     const copyTaches = [...taches];
-    copyTaches.splice(id - 1, 1);
+    const indexDelete = copyTaches.indexOf(tache)
+    copyTaches.splice(indexDelete, 1);
     setTaches(copyTaches);
+  };
+
+  const handleChange = (event) => {
+    const valueAfterChange = event.target.value;
+    setNouvelleTache(valueAfterChange);
   };
 
   // Affichage
@@ -31,18 +45,19 @@ function App() {
       <h1>To Do List</h1>
       <form action="submit" onSubmit={addTaches}>
         <input
-          ref={inputRef}
+          value={nouvelleTache}
           type="text"
           placeholder="Entrez votre votre tache"
+          onChange={handleChange}
         />
         <button>Ajouter la tache</button>
       </form>
       <ul>
         {taches.map((tache) => {
           return (
-            <li key={tache.id}>
+            <li key={taches.indexOf(tache)}>
               <input type="checkbox" />
-              {tache.nom} <button onClick={() => deleteLi(tache.id)}>X</button>
+              {tache.nom} <button onClick={() => deleteLi(tache)}>X</button>
             </li>
           );
         })}
