972c85d560d383165b859275cdcf9b5d2ef46875
maxilaf <120785680+maxilaf@users.noreply.github.com>
2024-06-25 17:48:09 +0200

    Create api.js

---
+++ api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const [chiffre, setChiffre] = useState(null);
+    const [nom, setNom] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const handleRefresh = () => {
+        fetchData(); // Réexécute la fonction fetchData pour récupérer les données les plus récentes
+    };
+
+    // Fonction pour récupérer les données
+    const fetchData = () => {
+        // Appel à la route /api/data
+        axios.get('http://127.0.0.1:5000/api/chiffre')
+            .then(response => {
+                setChiffre(response.data);
+        })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des données:', error);
+        });
+
+        axios.get('http://127.0.0.1:5000/api/nom')
+            .then(response => {
+                setNom(response.data);
+            })
+            .catch(error => {
+                console.error('Erreur lors de la récupération des données:', error);
+            });
+        
+        
+    };
